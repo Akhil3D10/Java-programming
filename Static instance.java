2ce2b5c1b960440e92ea2ca43c75ce437b4b2bb5
Akhil3D10 <142907948+Akhil3D10@users.noreply.github.com>
2024-05-12 10:03:09 +0530

    Update Staticinstance.java

--- Static instance.java
+++ Static instance.java
@@ -1,9 +1,18 @@
-class StaticInstance{
-int a=35;
-static int b=55;
-void display (){
-System.out.println("instance method");
-}
-static void display(){
-System.out.println("static method");
+public class StaticInstance{
+    int a=35;
+    static int b=55;
+    void display(){
+    System.out.println("instance method");
+    }
+    static void display1(){
+    System.out.println("static method");
+    } 
+    public static void main(String[] args) {
+        System.out.println(b);
+        StaticInstance si = new StaticInstance();
+        System.out.println(si.a);
+        si.display();
+        display1();
+    }
+    
 }
