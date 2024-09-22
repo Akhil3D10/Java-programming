c73c9b157bb747e4164f39803aab93cb96ee40bb
akhil <akhilpolamoni1458@gmail.com>
2024-09-22 18:31:59 +0530

    new html file added

--- overriding.java
+++ overriding.java
@@ -1,13 +1,16 @@
-class A{
-public void action(){
-System.out.println("this is parent");
+class A {
+    public void action() {
+        System.out.println("this is parent");
+    }
 }
-}
-class B extends A{
-public void action(){
-System.out.println("this is child and method is overridden");
-}
-public static void main(String args[]){
-B bObj = new B();
-b.action();
+
+class B extends A {
+    public void action() {
+        System.out.println("this is child and method is overridden");
+    }
+
+    public static void main(String args[]) {
+        B bObj = new B();
+        bObj.action();
+    }
 }
--- polymorphism.java
+++ polymorphism.java
@@ -1,36 +1,41 @@
-class parent{
-    public void action(){
-        System.out.println("the parent class");
+class parent {
+    public void action() {
+        System.out.println("this is the parent class");
     }
-    public void display(){
+
+    public void display() {
         System.out.println("other method in parent class");
     }
 }
-class child extends parent{
-    public void action(){
+
+class child extends parent {
+    public void action() {
         System.out.println("chils class method");
     }
 }
-class overloading{
-    public void add(int a, int b){
-        System.out.println(a+b);
+
+class overloading {
+    public void add(int a, int b) {
+        System.out.println(a + b);
     }
-    public void add(float a, float b){
-        System.out.println(a+b);
+
+    public void add(float a, float b) {
+        System.out.println(a + b);
     }
 }
+
 public class polymorphism {
     public static void main(String[] args) {
-//---------------------------Method Overriding---------------------------------
+        // ---------------------------Method Overriding---------------------------------
 
         child cobj = new child();
         cobj.action();
         cobj.display();
-//---------------------------Method Overloading-------------------------------
+        // ---------------------------Method Overloading-------------------------------
 
         overloading ovobj = new overloading();
-        ovobj.add(10,12);
-        ovobj.add(13.55f,12.45f);
+        ovobj.add(10, 12);
+        ovobj.add(13.55f, 12.45f);
     }
-    
+
 }
